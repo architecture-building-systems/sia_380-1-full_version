4230082b8858b40ce1da491165968f6ebaff5cf0
walkerl <linwal@bluewin.ch>
2020-02-10 10:09:57 +0100

    first commit for sensitivity analysis

---
+++ sensitivity.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import simulation_engine as se
+
+### Erforderliche Nutzereingaben:
+gebaeudekategorie_sia = 1.1
+regelung = "andere"  # oder "Referenzraum" oder "andere"
+hohe_uber_meer = 435.0 # Eingabe
+energiebezugsflache = 2275.0  # m2
+anlagennutzungsgrad_wrg = 0.0 ## SIA 380-1 Tab 23
+warmespeicherfahigkeit_pro_EBF = 0.08 ## Wert noch nicht klar, bestimmen gemäss SN EN ISO 13786 oder Tab25
+korrekturfaktor_luftungs_eff_f_v = 1.0  # zwischen 0.8 und 1.2 gemäss SIA380-1 Tab 24
+infiltration_volume_flow = 0.15  # Gemäss SIA 380-1 2016 3.5.5 soll 0.15m3/(hm2) verwendet werden. Korrigenda anschauen
+
+
+## Gebäudehülle
+u_windows = 0.74
+u_walls = 0.12
+u_roof = 0.11
+u_floor = 0.13
+b_floor = 0.4
+
+
+## Systeme
+heizsystem = "HP"
+dhw_heizsystem = "HP"
+
+
+
+
+### Bauteile:
+## Windows: [[Orientation],[Areas],[U-value],[g-value]]
+windows = np.array([["N", "E", "S", "W"],
+                    [131.5, 131.5, 131.5, 131.5],
+                    [u_windows, u_windows, u_windows, u_windows],
+                    [0.6, 0.6, 0.6, 0.6]],
+                   dtype=object)  # dtype=object is necessary because there are different data types
+
+## walls: [[Areas], [U-values]]
+walls = np.array([[412.5, 412.5, 412.5, 412.5],
+                  [u_walls, u_walls, u_walls, u_walls]])
+
+## roof: [[Areas], [U-values]]
+roof = np.array([[506.0], [u_roof]])
+
+## floor to ground (for now) [[Areas],[U-values],[b-values]]
+floor = np.array([[506.0],[u_floor],[b_floor]])
+
+
+Gebaeude_1 = se.Building(gebaeudekategorie_sia, regelung, windows, walls, roof, floor, energiebezugsflache,
+                         anlagennutzungsgrad_wrg, infiltration_volume_flow, warmespeicherfahigkeit_pro_EBF,
+                         korrekturfaktor_luftungs_eff_f_v, hohe_uber_meer)
+
+Gebaeude_1.run_SIA_380_1()
+print(Gebaeude_1.heizwarmebedarf.sum())  #kWh/m2a
+
+
+
+## Gebäudedimensionen
+Gebaeude_1.heating_system = heizsystem
+# Gebaeude_1.electricity_mix =
+Gebaeude_1.dhw_heating_system = dhw_heizsystem  ## Achtung, momentan ist der COP für DHW und für Heizung gleich.
+
+
+Gebaeude_1.run_SIA_380_emissions()
+print(Gebaeude_1.emissions.sum())  # CO2eq/m2a
+print(Gebaeude_1.non_renewable_primary_energy.sum())  # kWh/m2a
+
+quit()
+##### Plots:
+
+# plt.plot(transmissionsverluste, label="Transmissionswärmeverluste")
+# plt.plot(luftungsverluste, label="Lüftungsverlust")
+plt.plot(gesamtwarmeverluste, label="Gesamtwärmeverluste")
+# plt.plot(interne_eintrage, label="Interne Wärmegewinne")
+# plt.plot(solare_eintrage, label="Solare Wärmegewinne")
+plt.plot(totale_warmeeintrage, label="Totale Wärmegewinne")
+# plt.plot(genutzte_warmeeintrage, label="Genutzte Wärmegewinne")
+plt.plot(heizwarmebedarf, label="Heizwärmebedarf")
+plt.legend()
+plt.ylabel("kWh/(m2a)")
+plt.title("SIA 380-1 monatliche Bilanz")
+plt.show()
+
+print("Annual Heizwärmebedarf", sum(heizwarmebedarf))
