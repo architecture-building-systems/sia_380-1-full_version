f2cfcb0f6e18643442d2244a9713115f31a978ff
walkerl <linwal@bluewin.ch>
2020-02-10 14:47:48 +0100

    sensitivity setup with plots, so far no surprises

--- sensitivity.py
+++ sensitivity.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import plotly.graph_objects as go
+
 import simulation_engine as se
 
 from SALib.sample import saltelli
 from SALib.analyze import sobol
-
 
 
 ### Erforderliche Nutzereingaben:
@@ -12,32 +14,31 @@ gebaeudekategorie_sia = 1.1
 regelung = "andere"  # oder "Referenzraum" oder "andere"
 hohe_uber_meer = 435.0 # Eingabe
 energiebezugsflache = 2275.0  # m2
-anlagennutzungsgrad_wrg = 0.0 ## SIA 380-1 Tab 23
-warmespeicherfahigkeit_pro_EBF = 0.08 ## Wert noch nicht klar, bestimmen gemäss SN EN ISO 13786 oder Tab25
-korrekturfaktor_luftungs_eff_f_v = 1.0  # zwischen 0.8 und 1.2 gemäss SIA380-1 Tab 24
+# anlagennutzungsgrad_wrg = 0.0 ## SIA 380-1 Tab 23
+# warmespeicherfahigkeit_pro_EBF = 0.08 ## Wert noch nicht klar, bestimmen gemäss SN EN ISO 13786 oder Tab25
+# korrekturfaktor_luftungs_eff_f_v = 1.0  # zwischen 0.8 und 1.2 gemäss SIA380-1 Tab 24
 infiltration_volume_flow = 0.15  # Gemäss SIA 380-1 2016 3.5.5 soll 0.15m3/(hm2) verwendet werden. Korrigenda anschauen
 
-
-## Gebäudehülle
-u_roof = 0.11
-u_floor = 0.13
-b_floor = 0.4
 
 
 
 
 ### Generate Samples
 problem = {
-    'num_vars':6,
-    'names':['u_walls', 'u_windows', 'g_windows'],
-    'bounds':[[0.12, 0.25],  # u_walls
+    'num_vars':10,
+    'names':['u_walls', 'u_windows', 'g_windows', 'u_floor', 'b_floor', 'u_roof', 'thermal_mass', 'f_v', 'eta_g', 'infiltration_volume_flow'],
+    'bounds':[[0.12, 0.4],  # u_walls
               [0.75, 1.3],  # u_windows
-              [0.2, 0.6],  # g_windows
+              [0.3, 0.6],  # g_windows
               [0.08, 0.25],  # u_floor
               [0.1, 1.0],  # b_floor
               [0.08, 0.25],  # u_roof
+              [0.03, 0.15],  # waermespeicherfaehigkeit pro EBF
+              [0.8, 1.2],  # f_v
+              [0.0, 0.70],  # eta g, heat recovery efficiency
+              [0.05, 0.25]  # infiltration volume flow
               ]}
-param_values = saltelli.sample(problem, 10000)
+param_values = saltelli.sample(problem, 500)
 
 
 ### Run Model
@@ -45,12 +46,19 @@ param_values = saltelli.sample(problem, 10000)
 Y = np.zeros([param_values.shape[0]])
 for i, X in enumerate(param_values):
 
+    # print(i)  # Take this out for long simulations as it takes a lot of time. Otherwise, this is an indicator on the
+              # progress of the simulation.
+
     u_walls = X[0]
     u_windows = X[1]
     g_windows = X[2]
     u_floor = X[3]
     b_floor = X[4]
     u_roof = X[5]
+    warmespeicherfahigkeit_pro_EBF = X[6]
+    korrekturfaktor_luftungs_eff_f_v = X[7]  # zwischen 0.8 und 1.2 gemäss SIA380-1 Tab 24
+    anlagennutzungsgrad_wrg = X[8]
+    infiltration_volume_flow = X[9]
 
 
     ## Systeme
@@ -91,10 +99,12 @@ for i, X in enumerate(param_values):
 
     Y[i] = Gebaeude_1.heizwarmebedarf.sum()  #kWh/m2a
 
-Si = sobol.analyze(problem, Y)
+print("sobol analysis...")
+Si = sobol.analyze(problem, Y)  #, parallel=True, n_processors=6 )
 
 print(Si['S1'])
 print(Si['S2'])
+print(Si['ST'])
 
 # print("x1-x2:", Si['S2'][0,1])
 # print("x1-x3:", Si['S2'][0,2])
@@ -102,22 +112,13 @@ print(Si['S2'])
 #
 
 
-
-
-quit()
-##### Plots:
-
-# plt.plot(transmissionsverluste, label="Transmissionswärmeverluste")
-# plt.plot(luftungsverluste, label="Lüftungsverlust")
-plt.plot(gesamtwarmeverluste, label="Gesamtwärmeverluste")
-# plt.plot(interne_eintrage, label="Interne Wärmegewinne")
-# plt.plot(solare_eintrage, label="Solare Wärmegewinne")
-plt.plot(totale_warmeeintrage, label="Totale Wärmegewinne")
-# plt.plot(genutzte_warmeeintrage, label="Genutzte Wärmegewinne")
-plt.plot(heizwarmebedarf, label="Heizwärmebedarf")
-plt.legend()
-plt.ylabel("kWh/(m2a)")
-plt.title("SIA 380-1 monatliche Bilanz")
+plt.bar(problem['names'], Si['ST'])
 plt.show()
 
-print("Annual Heizwärmebedarf", sum(heizwarmebedarf))
+plt.pcolormesh(Si['S2'], cmap='binary')
+plt.colorbar()
+plt.xticks(np.arange(0.5,11.5,1.0), problem['names'])
+plt.yticks(np.arange(0.5,11.5,1.0), problem['names'])
+plt.show()
+
+quit()
