778c0105625be6e3259cb1f87a380e725422cfc5
walkerl <linwal@bluewin.ch>
2020-06-15 17:15:04 +0200

    version with performance matrix save to excel

--- simulation_coordination.py
+++ simulation_coordination.py
@@ -14,12 +14,15 @@ Im this first part of the code, building, its location and all the related syste
 """
 scenarios_path = r"C:\Users\walkerl\Documents\code\sia_380-1-full_version\data\scenarios.xlsx"
 configurations_path = r"C:\Users\walkerl\Documents\code\sia_380-1-full_version\data\configurations.xlsx"
+performance_matrix_path = r"C:\Users\walkerl\Documents\code\sia_380-1-full_version\data\performance_matrix.xlsx"
 
 
 scenarios = pd.read_excel(scenarios_path)
 configurations = pd.read_excel(configurations_path, index_col="Configuration", skiprows=[1])
 
-for index, config in configurations.iterrows():
+emission_performance_matrix = np.empty((len(configurations.index), len(scenarios.index)))
+
+for config_index, config in configurations.iterrows():
 
 
     ## Erforderliche Nutzereingaben:
@@ -30,7 +33,7 @@ for index, config in configurations.iterrows():
     anlagennutzungsgrad_wrg = 0.0 ## SIA 380-1 Tab 23
     warmespeicherfahigkeit_pro_EBF = config['thermal mass per erf'] ## Wert noch nicht klar, bestimmen gemäss SN EN ISO 13786 oder Tab25 Einheiten?
     korrekturfaktor_luftungs_eff_f_v = 1.0  # zwischen 0.8 und 1.2 gemäss SIA380-1 Tab 24
-    infiltration_volume_flow = config['infiltration volume flow']  # Gemäss SIA 380-1 2016 3.5.5 soll 0.15m3/(hm2) verwendet werden. Korrigenda anschauen
+    infiltration_volume_flow_planned = config['infiltration volume flow']  # Gemäss SIA 380-1 2016 3.5.5 soll 0.15m3/(hm2) verwendet werden. Korrigenda anschauen
     ventilation_volume_flow = config['ventilation volume flow'] # give a number in m3/(hm2) or select "SIA" to follow SIA380-1 code
     heating_setpoint = config['heating setpoint']  # give a number in deC or select "SIA" to follow the SIA380-1 code
     cooling_setpoint = config['cooling setpoint'] # give a number in deC or select "SIA" to follow the SIA380-1 code
@@ -79,172 +82,175 @@ for index, config in configurations.iterrows():
 
     # roof: [[Areas], [U-values]]
     roof = np.array([[config["roof area"]], [u_roof]])
-    print(roof)
 
     # floor to ground (for now) [[Areas],[U-values],[b-values]]
     floor = np.array([[config["floor area"]], [u_floor],[b_floor]])
 
-    print("Configuration %s prepared" %(index))
+    print("Configuration %s prepared" %config_index)
+
+    for scenario_index, scenario in scenarios.iterrows():
+
+        weatherfile_path = scenario["weatherfile"]
+        occupancy_path = scenario['occupancy schedule']
+
+        weather_data_sia = dp.epw_to_sia_irrad(weatherfile_path)
+        infiltration_volume_flow = infiltration_volume_flow_planned * scenario['infiltration volume flow factor']  # This accounts for improper construction/tightness
 
 
-    print(type(config['roof area']))
-    # quit()
+        """
+        ###################################### SYSTEM SIMULATION ###############################################################
+        In this part the performance simulation is happening in three steps:
+            1. An hourly time series for PV yield ist calculated
+            2. A demand time series for DHW is calculated
+            3. A demand time series for room heating is calculated
+            4. A demand time series for room cooling is calculated (at the moment only for dynamic model)
+            5. Operational emissions based on final electricity demand and other heat sources is calculated in the respective
+               model time resolution.
+        
+        These steps are either carried out in the dynamic or in the static model. This is chosen above.       
+        """
 
-    weatherfile_path = r"C:\Users\walkerl\polybox\phd\Validation\ASHRAE140\140-2017-AccompanyingFiles\DRYCOLD.epw"
-
-    weather_data_sia = dp.epw_to_sia_irrad(weatherfile_path)
-    occupancy_path = r"C:\Users\walkerl\Documents\code\RC_BuildingSimulator\rc_simulator\auxiliary\occupancy_office.csv"
-
-    infiltration_volume_flow = 1.35  # Gemäss SIA 380-1 2016 3.5.5 soll 0.15m3/(hm2) verwendet werden. Korrigenda anschauen
-    ventilation_volume_flow = 0.0  # give a number in m3/(hm2) or select "SIA" to follow SIA380-1 code
+        ## PV calculation
+        # pv yield in kWh for each hour
+        #TODO check if the further functions also use kWh
+        pv_yield_hourly = dp.photovoltaic_yield_hourly(pv_azimuth, pv_tilt, pv_efficiency, pv_performance_ratio, pv_area,
+                                      weatherfile_path)
 
 
+        ## heating demand and emission calculation
 
-    """
-    ###################################### SYSTEM SIMULATION ###############################################################
-    In this part the performance simulation is happening in three steps:
-        1. An hourly time series for PV yield ist calculated
-        2. A demand time series for DHW is calculated
-        3. A demand time series for room heating is calculated
-        4. A demand time series for room cooling is calculated (at the moment only for dynamic model)
-        5. Operational emissions based on final electricity demand and other heat sources is calculated in the respective
-           model time resolution.
+        Gebaeude_static = se.Building(gebaeudekategorie_sia, regelung, windows, walls, roof, floor, energiebezugsflache,
+                                 anlagennutzungsgrad_wrg, infiltration_volume_flow, ventilation_volume_flow,
+                                 warmespeicherfahigkeit_pro_EBF, korrekturfaktor_luftungs_eff_f_v, hohe_uber_meer,
+                                      heating_setpoint, cooling_setpoint, area_per_person)
+
+        Gebaeude_static.pv_production = pv_yield_hourly
+        Gebaeude_static.run_SIA_380_1(weather_data_sia)
+        Gebaeude_static.run_ISO_52016_monthly(weather_data_sia)
+
+
+        ## Gebäudedimensionen
+        Gebaeude_static.heating_system = heizsystem
+        Gebaeude_static.dhw_heating_system = dhw_heizsystem  ## Achtung, momentan ist der COP für DHW und für Heizung gleich.
+        Gebaeude_static.cooling_system = cooling_system  # Diese Definitionens sollten verschoben werden zur definition des Objekts
+        Gebaeude_static.run_dhw_demand()
+
+
+        # print("heating SIA")
+        # print(Gebaeude_static.heizwarmebedarf.sum())
+
+        # print("cooling SIA")
+        # print(Gebaeude_static.monthly_cooling_demand.sum())
+
+        Gebaeude_static.run_SIA_electricity_demand(occupancy_path)
+
+        Gebaeude_static.run_SIA_380_emissions(emission_factor_type="SIA_380", avg_ashp_cop=2.8)
+
+        # print("operational emissions static SIA")
+        # print(Gebaeude_static.operational_emissions.sum())  # CO2eq/m2a
+
+        emission_performance_matrix[config_index, scenario_index] = Gebaeude_static.operational_emissions.sum()
+
+
+        # print(Gebaeude_static.non_renewable_primary_energy.sum())  # kWh/m2a
+
+
+        """
+
+        Gebaeude_dyn = sime.Sim_Building(gebaeudekategorie_sia, regelung, windows, walls, roof, floor, energiebezugsflache,
+                                       anlagennutzungsgrad_wrg, infiltration_volume_flow, ventilation_volume_flow,
+                                       warmespeicherfahigkeit_pro_EBF,
+                                       korrekturfaktor_luftungs_eff_f_v, hohe_uber_meer, heizsystem, cooling_system,
+                                       dhw_heizsystem, heating_setpoint, cooling_setpoint, area_per_person)
+
+        Gebaeude_dyn.pv_production = pv_yield_hourly
+
+        Gebaeude_dyn.run_rc_simulation(weatherfile_path=weatherfile_path,
+                                     occupancy_path=occupancy_path)
+
+
+
+        print("Heating RC")
+        print((dp.hourly_to_monthly(Gebaeude_dyn.heating_demand) / 1000.0 / energiebezugsflache).sum())
+        # print("DHW")
+        # print(dp.hourly_to_monthly(Gebaeude_dyn.dhw_demand)/1000.0 / energiebezugsflache)
+        print("cooling RC")
+        print(dp.hourly_to_monthly((Gebaeude_dyn.cooling_demand)/ 1000.0 / energiebezugsflache).sum())
+
+        Gebaeude_dyn.run_SIA_electricity_demand(occupancy_path)
+        Gebaeude_dyn.run_dynamic_emissions("SIA_380", "c")
+        #
+        print("operational emissions dynamic RC")
+        print(Gebaeude_dyn.operational_emissions.sum() / energiebezugsflache)
+        # print(dp.hourly_to_monthly((Gebaeude_1.heating_emissions + Gebaeude_1.dhw_emisions) / 1000.0 / energiebezugsflache))
+
+        # else:
+        #     print("simulation type not correctly specified")
+
+        """
+
+        """####################################
     
-    These steps are either carried out in the dynamic or in the static model. This is chosen above.       
-    """
-
-    ## PV calculation
-    # pv yield in kWh for each hour
-    #TODO check if the further functions also use kWh
-    pv_yield_hourly = dp.photovoltaic_yield_hourly(pv_azimuth, pv_tilt, pv_efficiency, pv_performance_ratio, pv_area,
-                                  weatherfile_path)
-
-
-    ## heating demand and emission calculation
-
-    Gebaeude_static = se.Building(gebaeudekategorie_sia, regelung, windows, walls, roof, floor, energiebezugsflache,
-                             anlagennutzungsgrad_wrg, infiltration_volume_flow, ventilation_volume_flow,
-                             warmespeicherfahigkeit_pro_EBF, korrekturfaktor_luftungs_eff_f_v, hohe_uber_meer,
-                                  heating_setpoint, cooling_setpoint, area_per_person)
-
-    Gebaeude_static.pv_production = pv_yield_hourly
-    Gebaeude_static.run_SIA_380_1(weather_data_sia)
-    Gebaeude_static.run_ISO_52016_monthly(weather_data_sia)
-
-
-    ## Gebäudedimensionen
-    Gebaeude_static.heating_system = heizsystem
-    Gebaeude_static.dhw_heating_system = dhw_heizsystem  ## Achtung, momentan ist der COP für DHW und für Heizung gleich.
-    Gebaeude_static.cooling_system = cooling_system  # Diese Definitionens sollten verschoben werden zur definition des Objekts
-    Gebaeude_static.run_dhw_demand()
-
-    print("heating")
-    print(Gebaeude_static.heizwarmebedarf.sum())
-
-    print("cooling")
-    print(Gebaeude_static.monthly_cooling_demand.sum())
 
 
 
-    Gebaeude_static.run_SIA_electricity_demand(occupancy_path)
-
-    Gebaeude_static.run_SIA_380_emissions(emission_factor_type="SIA_380", avg_ashp_cop=2.8)
-
-    print("operational emissions static")
-    print(Gebaeude_static.operational_emissions.sum())  # CO2eq/m2a
-
-    # print(Gebaeude_static.non_renewable_primary_energy.sum())  # kWh/m2a
-
-
-    Gebaeude_dyn = sime.Sim_Building(gebaeudekategorie_sia, regelung, windows, walls, roof, floor, energiebezugsflache,
-                                   anlagennutzungsgrad_wrg, infiltration_volume_flow, ventilation_volume_flow,
-                                   warmespeicherfahigkeit_pro_EBF,
-                                   korrekturfaktor_luftungs_eff_f_v, hohe_uber_meer, heizsystem, cooling_system,
-                                   dhw_heizsystem, heating_setpoint, cooling_setpoint, area_per_person)
-
-    Gebaeude_dyn.pv_production = pv_yield_hourly
-
-    Gebaeude_dyn.run_rc_simulation(weatherfile_path=weatherfile_path,
-                                 occupancy_path=occupancy_path)
+        ajajaj = zip(dp.hourly_to_monthly(Gebaeude_dyn.heating_demand) / 1000.0 / energiebezugsflache,
+                      dp.hourly_to_monthly(Gebaeude_dyn.dhw_demand)/1000.0 / energiebezugsflache,
+                      dp.hourly_to_monthly(Gebaeude_dyn.cooling_demand)/ 1000.0 / energiebezugsflache,
+                      Gebaeude_static.heizwarmebedarf,
+                      Gebaeude_static.dhw_demand,
+                      -Gebaeude_static.monthly_cooling_demand,
+                     Gebaeude_static.operational_emissions,
+                     dp.hourly_to_monthly(Gebaeude_dyn.operational_emissions)/energiebezugsflache)
 
 
-    print("Heating")
-    print((dp.hourly_to_monthly(Gebaeude_dyn.heating_demand) / 1000.0 / energiebezugsflache).sum())
-    # print("DHW")
-    # print(dp.hourly_to_monthly(Gebaeude_dyn.dhw_demand)/1000.0 / energiebezugsflache)
-    print("cooling")
-    print(dp.hourly_to_monthly((Gebaeude_dyn.cooling_demand)/ 1000.0 / energiebezugsflache).sum())
+        results = pd.DataFrame(ajajaj, columns=["RC heating", "RC DHW", "RC cooling", "380 heating", "380 DHW", "ISO cooling", "static_emissions", "dynamic emissions"])
+        # results["ISO2RC"] = results['ISO cooling']/results['RC cooling']
+        results["RC_solar_gains"] = dp.hourly_to_monthly(Gebaeude_dyn.solar_gains)/1000.0 / energiebezugsflache
+        results["ISO_solar_gains"] = Gebaeude_static.iso_solar_gains
+        results["SIA_solar_gains"] = Gebaeude_static.solare_eintrage
 
-    Gebaeude_dyn.run_SIA_electricity_demand(occupancy_path)
-    Gebaeude_dyn.run_dynamic_emissions("SIA_380", "c")
-    #
-    print("operational emissions dynamic")
-    print(Gebaeude_dyn.operational_emissions.sum() / energiebezugsflache)
-    # print(dp.hourly_to_monthly((Gebaeude_1.heating_emissions + Gebaeude_1.dhw_emisions) / 1000.0 / energiebezugsflache))
+        results["transmission_losses_ISO"] = Gebaeude_static.iso_transmission_losses
+        results["transmission_losses_SIA"] = Gebaeude_static.transmissionsverluste
 
-    # else:
-    #     print("simulation type not correctly specified")
-
-    """####################################
-    """
+        results["internal_gains_RC"] = dp.hourly_to_monthly(Gebaeude_dyn.internal_gains)/1000.0 /energiebezugsflache
+        results["internal_gains_SIA"] = Gebaeude_static.interne_eintrage
+        results["internal_gains_ISO"] = Gebaeude_static.iso_internal_gains
 
 
-
-    ajajaj = zip(dp.hourly_to_monthly(Gebaeude_dyn.heating_demand) / 1000.0 / energiebezugsflache,
-                  dp.hourly_to_monthly(Gebaeude_dyn.dhw_demand)/1000.0 / energiebezugsflache,
-                  dp.hourly_to_monthly(Gebaeude_dyn.cooling_demand)/ 1000.0 / energiebezugsflache,
-                  Gebaeude_static.heizwarmebedarf,
-                  Gebaeude_static.dhw_demand,
-                  -Gebaeude_static.monthly_cooling_demand,
-                 Gebaeude_static.operational_emissions,
-                 dp.hourly_to_monthly(Gebaeude_dyn.operational_emissions)/energiebezugsflache)
+        results[["RC_solar_gains", "ISO_solar_gains", "SIA_solar_gains"]].plot(kind='bar', title="Monthly Solar Gains")
+        plt.ylabel("Solar Gains [kWh/m2M]")
+        plt.show()
 
 
-    results = pd.DataFrame(ajajaj, columns=["RC heating", "RC DHW", "RC cooling", "380 heating", "380 DHW", "ISO cooling", "static_emissions", "dynamic emissions"])
-    # results["ISO2RC"] = results['ISO cooling']/results['RC cooling']
-    results["RC_solar_gains"] = dp.hourly_to_monthly(Gebaeude_dyn.solar_gains)/1000.0 / energiebezugsflache
-    results["ISO_solar_gains"] = Gebaeude_static.iso_solar_gains
-    results["SIA_solar_gains"] = Gebaeude_static.solare_eintrage
+        results[["internal_gains_RC", "internal_gains_SIA", "internal_gains_ISO"]].plot(kind='bar', title="Internal Gains")
+        plt.ylabel("Internal Gains [kWh/m2M]")
+        plt.show()
 
-    results["transmission_losses_ISO"] = Gebaeude_static.iso_transmission_losses
-    results["transmission_losses_SIA"] = Gebaeude_static.transmissionsverluste
+        plt.plot(Gebaeude_dyn.cooling_demand/1000.0/energiebezugsflache, label="Cooling")
+        plt.plot(Gebaeude_dyn.heating_demand/1000.0/energiebezugsflache, label="Heating")
+        plt.ylabel("Energy / Power [kWh/m2h]")
+        plt.legend()
+        plt.show()
 
-    results["internal_gains_RC"] = dp.hourly_to_monthly(Gebaeude_dyn.internal_gains)/1000.0 /energiebezugsflache
-    results["internal_gains_SIA"] = Gebaeude_static.interne_eintrage
-    results["internal_gains_ISO"] = Gebaeude_static.iso_internal_gains
+        results[["transmission_losses_ISO", "transmission_losses_SIA"]].plot(kind='bar', title="Transmission Losses")
+        plt.ylabel("Monthly Transmission Losses [kWh/m2M]")
+        plt.show()
 
+        results[["RC heating", "RC DHW", "RC cooling", "380 heating", "380 DHW", "ISO cooling"]].plot(kind="bar", title="Energy Demand")
+        plt.ylabel("Energy demand for heating, cooling and DHW [kWh/m2M]")
+        plt.show()
 
-    results[["RC_solar_gains", "ISO_solar_gains", "SIA_solar_gains"]].plot(kind='bar', title="Monthly Solar Gains")
-    plt.ylabel("Solar Gains [kWh/m2M]")
-    plt.show()
-
-
-    results[["internal_gains_RC", "internal_gains_SIA", "internal_gains_ISO"]].plot(kind='bar', title="Internal Gains")
-    plt.ylabel("Internal Gains [kWh/m2M]")
-    plt.show()
-
-    plt.plot(Gebaeude_dyn.cooling_demand/1000.0/energiebezugsflache, label="Cooling")
-    plt.plot(Gebaeude_dyn.heating_demand/1000.0/energiebezugsflache, label="Heating")
-    plt.ylabel("Energy / Power [kWh/m2h]")
-    plt.legend()
-    plt.show()
-
-    results[["transmission_losses_ISO", "transmission_losses_SIA"]].plot(kind='bar', title="Transmission Losses")
-    plt.ylabel("Monthly Transmission Losses [kWh/m2M]")
-    plt.show()
-
-    results[["RC heating", "RC DHW", "RC cooling", "380 heating", "380 DHW", "ISO cooling"]].plot(kind="bar", title="Energy Demand")
-    plt.ylabel("Energy demand for heating, cooling and DHW [kWh/m2M]")
-    plt.show()
-
-    results[["static_emissions", "dynamic emissions"]].plot(kind="bar", title="Operational Emissions")
-    plt.show()
+        results[["static_emissions", "dynamic emissions"]].plot(kind="bar", title="Operational Emissions")
+        plt.show()
 
 
 
-    """
-    ###################################### EMBODIED EMISSIONS ##############################################################
-    In this part the embodied emissions of a respective system and its components are defined.
     
-    """
+        ###################################### EMBODIED EMISSIONS ##############################################################
+        In this part the embodied emissions of a respective system and its components are defined.
+        
+        """
 
+print(emission_performance_matrix)
+pd.DataFrame(emission_performance_matrix, index=configurations.index, columns=scenarios.index).to_excel(performance_matrix_path)
