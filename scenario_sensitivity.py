f51c7e0a67632b07fc1849b292023d24cb6ddfc9
walkerl <linwal@bluewin.ch>
2020-06-22 15:38:09 +0200

    updated sensitivity analysis for A) configurations and B) scenarios. So far only implemented with monthly simulation.

--- scenario_sensitivity.py
+++ scenario_sensitivity.py
@@ -9,8 +9,7 @@ import simulation_engine_dynamic as sime
 from SALib.sample import saltelli
 from SALib.analyze import sobol
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
 
     """
     ###################################### SYSTEM DEFINITION ###############################################################
@@ -23,6 +22,32 @@ if __name__=='__main__':
     scenarios = pd.read_excel(scenarios_path)
     configurations = pd.read_excel(configurations_path, index_col="Configuration", skiprows=[1])
 
+    gebaeudekategorie_sia = 1.1
+    regelung = "andere"  # oder "Referenzraum" oder "andere"
+    hohe_uber_meer = 435.0  # Eingabe
+    energiebezugsflache = 2275.0  # m2
+    ventilation_volume_flow = 2.1  # give a number in m3/(hm2) or select "SIA" to follow SIA380-1 code
+    korrekturfaktor_luftungs_eff_f_v = 1.0
+    b_floor = 0.4
+    u_floor = 0.23
+    u_roof = 0.14
+    u_walls = 0.14
+    u_windows = 0.85
+    g_windows = 0.5
+    heating_setpoint = "SIA"
+    pv_azimuth = 180
+    cooling_setpoint = "SIA"
+    warmespeicherfahigkeit_pro_EBF = 0.08
+    pv_efficiency = 0.18  # zwischen 0.8 und 1.2 gemäss SIA380-1 Tab 24
+    anlagennutzungsgrad_wrg = 0.0
+    infiltration_volume_flow = 0.15
+    heizsystem = "ASHP"
+    dhw_heizsystem = heizsystem  ## This is currently a limitation of the RC Model. Automatically the same!
+    cooling_system = "GSHP"  # Only affects dynamic calculation. Static does not include cooling
+    pv_performance_ratio = 0.8
+    pv_area = energiebezugsflache  # m2, can be directly linked with roof size
+    pv_tilt = 30  # in degrees
+
     ## LCA angaben
     electricity_factor_source = "SIA"  # Can be "SIA", "eu", "empa_ac"
     electricity_factor_type = "annual"  # Can be "annual", "monthly", "hourly" (Hourly will only work for hourly model and
@@ -31,66 +56,45 @@ if __name__=='__main__':
 
 
 
-
     ### Generate Samples
     problem = {
-        'num_vars':11,
-        'names':['u_opaque', 'u_glazing', 'g_windows', 'heating_setpoint', 'PV azimuth', 'cooling_setpoint', 'thermal_mass',
-                 'pv_effieicncy', 'eta_g', 'infiltration_flow', 'heating system'],
-        'bounds':[[0.12, 0.4],  # u_walls
-                  [0.75, 2.0],  # u_windows
-                  [0.2, 0.8],  # g_windows
-                  [17.0, 25.0],  # heating_setpoint
-                  [0.0, 359.0],  # PV azimuth
-                  [19.0, 30.0],  # cooling_setpoint
-                  [0.03, 0.15],  # waermespeicherfaehigkeit pro EBF
-                  [0.10, 0.30],  # pv_efficiency
-                  [0.0, 0.70],  # eta g, heat recovery efficiency
-                  [0.05, 0.25],  # infiltration volume flow
-                  [0.5, 5.5]]}   # Heating system ## Abklären, ob dies so gemacht werden kann für diskretisierte Variablen.
+        'num_vars': 4,
+        'names': ['weather_file', 'infiltration_flow', 'occupancy_schedule', 'area_per_person'],
+        'bounds': [[0.5, 4.5],
+                   [0.05, 0.50],
+                   [0.5, 2.5],
+                   [20.0, 150.0]]}  # Heating system ## Abklären, ob dies so gemacht werden kann für diskretisierte Variablen.
     # "Natural Gas":0.249, "Wood":0.020, "Pellets":0.048, "GSHP_CH_mix":0.055, "ASHP_CH_mix":0.076, "GSHP_EU_mix":0.207, "ASHP_EU_mix":0.285
-    param_values = saltelli.sample(problem, 30)
+    param_values = saltelli.sample(problem, 3)
 
 
-    gebaeudekategorie_sia = 1.1
-    regelung = "andere"  # oder "Referenzraum" oder "andere"
-    hohe_uber_meer = 435.0  # Eingabe
-    energiebezugsflache = 2275.0  # m2
-    ventilation_volume_flow = 2.1  # give a number in m3/(hm2) or select "SIA" to follow SIA380-1 code
-    area_per_person = "SIA"  # give a number or select "SIA" to follow the SIA380-1 code (typical for MFH 40)
-    korrekturfaktor_luftungs_eff_f_v = 1.0
-    b_floor = 0.4
+    number_to_climate = {1: r"C:\Users\walkerl\Documents\code\sia_380-1-full_version\data\Zürich-hour_historic.epw",
+                         2: r"C:\Users\walkerl\Documents\code\sia_380-1-full_version\data\Zürich-2040-A1B.epw",
+                         3: r"C:\Users\walkerl\Documents\code\sia_380-1-full_version\data\Zürich-2070-A1B.epw",
+                         4: r"C:\Users\walkerl\Documents\code\sia_380-1-full_version\data\Zürich-2070-B1.epw"}
 
-
-    weatherfile_path = r"C:\Users\walkerl\Documents\code\sia_380-1-full_version\data\Zürich-hour_historic.epw"
-    weather_data_sia = dp.epw_to_sia_irrad(weatherfile_path)
-    occupancy_path = r"C:\Users\walkerl\Documents\code\RC_BuildingSimulator\rc_simulator\auxiliary\occupancy_office.csv"
-
+    number_to_occupancy = {1: r"C:\Users\walkerl\Documents\code\RC_BuildingSimulator\rc_simulator\auxiliary\occupancy_office.csv",
+                           2: r"C:\Users\walkerl\Documents\code\RC_BuildingSimulator\rc_simulator\auxiliary\occupancy_single_res.csv"
+                           }
 
 
     ### Run Model
     Y = np.zeros([param_values.shape[0]])
     for i, X in enumerate(param_values):
 
-        u_floor = u_roof = u_walls = X[0]
-        u_windows = X[1]
-        g_windows = X[2]
-        heating_setpoint = X[3]
-        pv_azimuth = X[4]
-        cooling_setpoint = X[5]
-        warmespeicherfahigkeit_pro_EBF = X[6]
-        pv_efficiency = X[7]  # zwischen 0.8 und 1.2 gemäss SIA380-1 Tab 24
-        anlagennutzungsgrad_wrg = X[8]
-        infiltration_volume_flow = X[9]
-        heating_system_number = np.round(X[10], 0)
-        number_to_system = {1:"Natural Gas", 2:"Wood", 3:"Pellets", 4:"GSHP", 5:"ASHP"}
 
-        heizsystem = number_to_system
-        dhw_heizsystem = heizsystem  ## This is currently a limitation of the RC Model. Automatically the same!
-        cooling_system = "GSHP"  # Only affects dynamic calculation. Static does not include cooling
-        pv_performance_ratio = 0.8
-        pv_area = energiebezugsflache  # m2, can be directly linked with roof size
-        pv_tilt = 30  # in degrees
+        climate_number = np.round(X[0], 0)
+        weatherfile_path = number_to_climate[climate_number]
+        weather_data_sia = dp.epw_to_sia_irrad(weatherfile_path)
+
+        infiltration_volume_flow=X[1]
+
+        occupancy_number = np.round(X[2],0)
+        occupancy_path = number_to_occupancy[occupancy_number]
+
+        area_per_person = X[3]
+
+
 
         ## Bauteile:
         # Windows: [[Orientation],[Areas],[U-value],[g-value]]
@@ -109,7 +113,6 @@ if __name__=='__main__':
 
         # floor to ground (for now) [[Areas],[U-values],[b-values]]
         floor = np.array([[energiebezugsflache], [u_floor], [b_floor]])
-
 
         ### Monatliche Berechnungen
         pv_yield_hourly = dp.photovoltaic_yield_hourly(pv_azimuth, pv_tilt, pv_efficiency, pv_performance_ratio,
@@ -130,22 +133,13 @@ if __name__=='__main__':
         Gebaeude_static.run_SIA_380_emissions(emission_factor_source=electricity_factor_source,
                                               emission_factor_type=electricity_factor_type, avg_ashp_cop=2.8)
 
-
-
         ### Stündliche Berechnungen:
 
-
-
-    # Y[i] = Gebaeude_1.heizwarmebedarf.sum()  #kWh/m2a
+        # Y[i] = Gebaeude_1.heizwarmebedarf.sum()  #kWh/m2a
         Y[i] = Gebaeude_static.operational_emissions.sum()
 
-
-
-
-
-
     print("sobol analysis...")
-    Si = sobol.analyze(problem, Y, parallel=True, n_processors=6 )
+    Si = sobol.analyze(problem, Y, parallel=True, n_processors=6)
 
     print(Si['S1'])
     print(Si['S2'])
@@ -156,15 +150,14 @@ if __name__=='__main__':
     # print("x2-x3:", Si['S2'][1,2])
     #
 
-
     plt.bar(problem['names'], Si['ST'])
     # plt.title('Sobol Sensitivities of Parameters for heating energy')
     plt.show()
 
     plt.pcolormesh(Si['S2'], cmap='binary')
     plt.colorbar()
-    plt.xticks(np.arange(0.5,12.5,1.0), problem['names'])
-    plt.yticks(np.arange(0.5,12.5,1.0), problem['names'])
+    plt.xticks(np.arange(0.5, 12.5, 1.0), problem['names'])
+    plt.yticks(np.arange(0.5, 12.5, 1.0), problem['names'])
     plt.show()
 
 
