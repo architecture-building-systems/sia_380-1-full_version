cfd1aa36d0628ef7df0e85b489ad894d014cd919
walkerl <linwal@bluewin.ch>
2020-06-24 21:54:02 +0200

    break

--- visualization.py
+++ visualization.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from pandas.plotting import parallel_coordinates
 
@@ -7,7 +8,7 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 10)
 
 
-classifier = "u-value wall"
+classifier = "heating system"
 
 
 scenarios_path = r"C:\Users\walkerl\Documents\code\sia_380-1-full_version\data\scenarios.xlsx"
@@ -20,6 +21,33 @@ configurations = pd.read_excel(configurations_path,  skiprows=[1])
 performance_matrix = pd.read_excel(performance_matrix_path, index_col="Configuration")
 
 
+print(configurations.index)
+print(scenarios.index)
+
+repeater = len(scenarios.index)
+
+all_configs = pd.DataFrame(configurations.values.repeat(repeater, axis=0), columns=configurations.columns)
+
+repeater = len(configurations.index)
+all_scenarios = pd.concat([scenarios]*repeater, ignore_index=True)
+
+all_data = pd.concat([all_configs, all_scenarios], axis=1)
+
+all_data.rename(columns={"Unnamed: 0":"Scenario"}, inplace=True)
+
+
+def func(x, y):
+    return performance_matrix[x][y]
+
+all_data["Performance"] = performance_matrix[all_data['Scenario']][all_data['Configuration']]
+
+
+print(all_data)
+
+
+
+
+"""
 all_data = pd.concat([configurations, performance_matrix], axis=1)
 scenario_list = list(scenarios.index)
 data_to_plot = all_data[scenario_list]
@@ -33,3 +61,4 @@ fig.subplots_adjust(top=0.93,wspace=0)
 pc = parallel_coordinates(data_to_plot, classifier)
 
 plt.show()
+"""
